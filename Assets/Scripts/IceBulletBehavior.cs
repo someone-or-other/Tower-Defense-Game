eb2ff821b078b168b34cc50269762d7cff22fc7a
PranavN1940 <pranavsurya.n@gmail.com>
2025-05-05 15:48:33 -0500

    Removed my work :(

--- Assets/Scripts/IceBulletBehavior.cs
+++ Assets/Scripts/IceBulletBehavior.cs
@@ -2,17 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IceBulletBehavior : MonoBehaviour
+public class IceBulletBehavior : BulletBehavior
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnTriggerEnter2D(Collider2D other)
     {
-        
-    }
+        if(other.CompareTag("Enemy"))
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
     }
 }
