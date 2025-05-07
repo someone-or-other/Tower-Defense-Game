e9d304b985f4d0e2e26a8af10dba409475b7ee63
PranavN1940 <pranavsurya.n@gmail.com>
2025-05-07 15:29:27 -0500

    EnemybuffScript completed

--- Assets/Scripts/EnemyBuffingTrigger.cs
+++ Assets/Scripts/EnemyBuffingTrigger.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class EnemyBuffingTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Enemy"))
+        {
+            other.getComponent<EnemyBehavior>().health += 2;
+            other.getComponent<EnemyBehavior>().enemyDamagePower += 5;
+            other.getComponent<EnemyBehavior>().speed += 2;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.getComponent<EnemyBehavior>().health -= 2;
+            other.getComponent<EnemyBehavior>().enemyDamagePower -= 5;
+            other.getComponent<EnemyBehavior>().speed -= 2;
+        }
     }
 }
